6688b61ab66cec98b5917060dde1ea4fe185a8f0
MarcinSkic <marcinskicoficjal@gmail.com>
2022-05-27 14:39:49 +0200

    [programming-paradigms] Finish extra homework

--- Python/Lab1.py
+++ Python/Lab1.py
@@ -1,5 +1,10 @@
 
 
+from locale import currency
+from logging import warning
+import re
+
+
 def FizzBuzz():
     for i in range (1,101):
         if (i%3 == 0 and i%5 == 0):
@@ -29,33 +34,15 @@ def DigitsIntoWordsImproved(number):
         print(words[i],end=" ")
 
 def NumberIntoRoman():
-    number = int(input("Podaj liczbę: "))
-    result = ""
-    symbols = {1000:'M',500:'D',100:'C',50:'L',10:'X',5:'V',1:'I'}
-    currentNumber = 1000
-    currentIndex = 0
-
-    while(number > 0):
-        if(number >= currentNumber):
-            result += symbols[currentNumber]
-            number -= currentNumber
-            print(number)
-            print(result)
-        else:
-            currentIndex += 1
-            currentNumber = list(symbols)[currentIndex]
-    print(result)
-
-def NumberIntoRomanImproved():
     symbols = {1000:'M',500:'D',100:'C',50:'L',10:'X',5:'V',1:'I'}
 
     number = int(input("Podaj liczbę: "))
-    currentNumber = 1000
     result = ""
-    for i in range(7):
+    for i in range(len(symbols)):
         currentNumber = list(symbols)[i]
         while(number >= currentNumber):
-            if(number >= currentNumber*9/5 and i%2 == 1):
+
+            if(number >= currentNumber*9/5 and i%2 == 1): #
                 result += symbols[list(symbols)[i+1]]
                 result += symbols[list(symbols)[i-1]]
                 number -= currentNumber*9/5
@@ -71,4 +58,41 @@ def NumberIntoRomanImproved():
             number -= currentNumber 
     print (result)
 
-NumberIntoRomanImproved()
+def NumberIntoRomanSlim():
+    symbols = {1000:'M',900:'CM',500:'D',400:'CD',100:'C',90:'XC',50:'L',40:'XL',10:'X',9:'IX',5:'V',4:'IV',1:'I'}
+
+    number = int(input("Podaj liczbę: "))
+    result = ""
+    for i in range(len(symbols)):
+        currentNumber = list(symbols)[i]
+        while(number >= currentNumber):
+
+            result += symbols[currentNumber]
+            number -= currentNumber 
+
+    print (result)
+
+def RomanIntoNumber():
+    symbols = {'M':1000,'D':500,'C':100,'L':50,'X':10,'V':5,'I':1}
+    number = input("Podaj liczbę rzymską: ")
+    result = 0
+    lastNumber = 1000
+    for char in number:
+        currentNumber = 0
+        
+        try:
+            currentNumber = symbols[char]
+        except KeyError: 
+            warning("Thats not roman numeral!")
+            return
+
+        if(lastNumber < currentNumber):
+            result += currentNumber-lastNumber*2
+        else:
+            result += currentNumber
+        lastNumber = currentNumber
+        
+        
+    print(result)
+
+RomanIntoNumber()
