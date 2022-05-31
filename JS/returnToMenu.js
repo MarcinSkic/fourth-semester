feaba8a68d6da2bca52382a55ec528a7e3590e1d
MarcinSkic <marcinskicoficjal@gmail.com>
2022-05-31 14:21:08 +0200

    [web-apps-basics] Add return to menu button

---
+++ JS/returnToMenu.js
@@ -0,0 +1,2 @@
+const body = document.querySelector('body');
+body.innerHTML += "<a href='../index.html' style='background-color: white; color: black; padding: 5px; position: absolute; bottom: 10px; left: 10px; border: 1px solid black;'>Wróć do menu</a>";
